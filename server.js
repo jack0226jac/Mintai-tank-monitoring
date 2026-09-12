e9702996c2280e531faed4aeb71f2f8fa8d87be2
jack0226jac <128864696+jack0226jac@users.noreply.github.com>
2026-09-12 19:26:49 +0800

    Update server.js

--- server.js
+++ server.js
@@ -33,11 +33,11 @@ app.post('/api/login', (req, res) => {
         validAccounts[username] === password
     ) {
 
-        // 登入成功後產生 8 小時有效的通行證
+        // 登入成功後產生 30分鐘有效的通行證
         const token = jwt.sign(
             { username: username },
             JWT_SECRET,
-            { expiresIn: '8h' }
+            { expiresIn: '30m' }
         );
 
         return res.json({
